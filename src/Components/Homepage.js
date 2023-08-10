9c2513039837c461bfc4a41c234a6560373cde47
JOSH <joshuadoe168@gmail>
2023-08-10 11:57:44 +0000

    first commit

---
+++ src/Components/Homepage.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import '../Components/Homepage.css'
+
+
+
+const Homepage = () => {
+    return (
+        <div className='home'>
+            <div className='content'>
+                
+                <p>“Balancing your< br />money is the key< br />to having enough.”</p>
+                <p>Doesn't come with instructions</p>
+                <button href='/' className='button'>Free Consultation</button>
+            </div>
+        </div>
+    )
+}
+
+export default Homepage;
+
+
+
+
+
+
+
+
+
+
